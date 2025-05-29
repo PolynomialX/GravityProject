b90861d25fbdb6f78b13dc5be08eab8483ac2e98
Robin H. <robin.holt@rocketmail.com>
2025-05-29 19:34:20 +0100

    Basic thoughts put into MathVec.

---
+++ MathematicsLib/MathVec.cpp
@@ -0,0 +1,12 @@
+#include "MathVec.h"
+template<typename T>
+MathVec<T>::MathVec()
+{
+
+}
+
+template<typename T>
+MathVec<T>::~MathVec()
+{
+    
+}
---
+++ MathematicsLib/MathVec.h
@@ -0,0 +1,36 @@
+#ifndef MATHVEC_H
+#define MATHVEC_H
+
+// We anticipate that this class will be
+// a base class
+
+// Custom mathematical vector library
+// Let's template it - we may want
+// to swap out different floating point 
+// types to compare performance or
+// precision...
+
+template <typename T>
+class MathVec
+{
+public:
+    // Default c'tor
+    MathVec();
+    // Currently torn between this being a base class
+    // and just having instances of MathVec for all quantities
+    ~MathVec();
+
+    // Calculate the Euclidean norm of the vector and return the value
+
+    // We need to overload the [] operator
+    
+private:
+    // Number of dimensions of the vector
+    size_t N;
+    // Magnitude of vector
+    T magnitude;
+    
+
+};
+
+#endif // MATHVEC_H
