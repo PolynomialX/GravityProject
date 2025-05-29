a29c721d31789c923c2b9f74f2289b021c3bc93e
Robin H. <robin.holt@rocketmail.com>
2025-05-29 22:00:43 +0100

    Initial work on MathVec with ideas commented.

--- src/MathematicsLib/MathVec.h
+++ src/MathematicsLib/MathVec.h
@@ -1,6 +1,8 @@
 #ifndef MATHVEC_H
 #define MATHVEC_H
-
+#include <memory>
+#include <iostream>
+#include <stdlib.h>
 // We anticipate that this class will be
 // a base class
 
@@ -16,20 +18,35 @@ class MathVec
 public:
     // Default c'tor
     MathVec();
+    // Construct a N_ by 1 zero vector
+    MathVec(size_t N_);
+    // Copy constructor
+    MathVec(const MathVec<T>& mathVec);
     // Currently torn between this being a base class
     // and just having instances of MathVec for all quantities
     ~MathVec();
 
     // Calculate the Euclidean norm of the vector and return the value
 
-    // We need to overload the [] operator
+    // Operators to overload: =,+,+=,-,[],/,* - this can rep
+    // both elt-wise mult. and dot prod??
+
+
+
+    // Getters & setters
+    const T& getN();
+    const T * const getElements();
 
 private:
     // Number of dimensions of the vector
-    size_t N;
+    size_t n;
     // Magnitude of vector
     unsigned double magnitude;
-    
+    // Raw pointer to elements
+    // Not using a smart pointer here as we 
+    // may want to employ IPP routines
+    // or try our hand at vectorising code...
+    T * elements;
 
 };
 
